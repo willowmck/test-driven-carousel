b222548f3995a0ded9f17e703807b3fcf85638b0
Will McKinley <wmckinle@redhat.com>
2020-01-23 19:35:15 -0500

    :wrench: Add Jest setup file

--- src/tests/CarouselButton.test.js
+++ src/tests/CarouselButton.test.js
@@ -1,10 +1,7 @@
 // src/tests/CarouselButton.test.js
 import React from 'react';
-import { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 import CarouselButton from '../CarouselButton';
-
-configure({ adapter: new Adapter() });
 
 describe('CarouselButton', () => {
   const text = 'Button text';

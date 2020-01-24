de9a0108837871f345f223e3ce0b3327063f866d
Will McKinley <wmckinle@redhat.com>
2020-01-24 11:22:06 -0500

    :wrench: Initial webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+// webpack.config.js
+module.exports = {
+  mode: 'development',
+  entry: {
+    carousel: './src/Carousel.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: require.resolve('babel-loader')
+      }
+    ]
+  }
+};

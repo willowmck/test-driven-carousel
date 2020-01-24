25d2dc67d168f5c4aea8b4b92a40772384f9f69b
Will McKinley <wmckinle@redhat.com>
2020-01-24 11:48:07 -0500

    :bulb: Add example page

---
+++ example/index.js
@@ -0,0 +1,9 @@
+// example/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Carousel from '../src/Carousel';
+import slides from './slides';
+
+const container = document.createElement('div');
+document.body.appendChild(container);
+ReactDOM.render(<Carousel slides={slides} />, container);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 // webpack.config.js
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    carousel: './src/Carousel.js'
+    carousel: './src/Carousel.js',
+    example: './example/index.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Carousel Example',
+      chunks: ['example']
+    })
+  ],
   module: {
     rules: [
       {
